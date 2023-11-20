7d1e409d3ffc26a7d310472c45fc8bd2720ccf9b
Marianagiacconi <marianagiacconisevilla@gmail.com>
2023-11-20 17:02:43 -0300

    fix

---
+++ testNg/src/test/java/LibraryTest.java
@@ -0,0 +1,10 @@
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertEquals;
+
+public class LibraryTest {
+
+    @Test
+    public void testLibraryFunctionality() {
+        assertEquals(1 + 1, 2);
+    }
+}
